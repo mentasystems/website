69dd833d20ad62ce7cd6c7f24bd435909feffc82
Olga Shestakova <olgashestakova@MacBook-Air.home>
2019-08-15 15:57:56 +0200

    side navbar change

--- src/components/NavBarMovileLink.jsx
+++ src/components/NavBarMovileLink.jsx
@@ -5,12 +5,19 @@ import '../styles/NavMobile.css'
 
 export function NavBarMobileLink(props) {
     return <div id="NavMobile_link_container">
-        <button id="NavMobile_btn_close" onClick={props.onClose}>x</button>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            marginRight: '20px'
+        }}>
+            <button id="NavMobile_btn_close" onClick={props.onClose}>x</button>
+        </div>
+
         <div className="NavMobile_links">
-            <a href="#texthard">Confianza</a>
-            <a href="#textcloud">Digitalización</a>
-            <a href="#textsecurity">Seguridad</a>
-            <a href="#Form_container">Contacto</a>
+            <a onClick={props.onClose} href="#texthard">Confianza</a>
+            <a onClick={props.onClose} href="#textcloud">Digitalización</a>
+            <a onClick={props.onClose} href="#textsecurity">Seguridad</a>
+            <a onClick={props.onClose} href="#Form_container">Contacto</a>
         </div>
     </div>
 }
